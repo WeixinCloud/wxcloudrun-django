62568e1f70e74896f23a069b63e0379ce22ec773
voyagerma <m17793873123@163.com>
2021-11-03 20:27:49 +0800

    统一RestAPI风格

--- wxcloudrun/views.py
+++ wxcloudrun/views.py
@@ -9,33 +9,63 @@ from django.forms.models import model_to_dict
 
 def create_or_update(request, _):
     if request.method == 'POST' or request.method == 'post':
-        return create(request)
+        try:
+            res = create(request)
+            return res
+        except Exception as e:
+            resp = {'code': 10000, 'errorMsg': 'create user error: {} '.format(str(e))}
+        return HttpResponse(json.dumps(resp), reason='Http Method Error', content_type='application/json')
 
     if request.method == 'PUT' or request.method == 'put':
-        return update_by_id(request)
+        try:
+            res = update_by_id(request)
+            return res
+        except Exception as e:
+            return HttpResponse(json.dumps({'code': 10000, 'errorMsg': 'update user error : {} '.format(str(e))}),
+                                content_type='application/json')
 
-    resp = {'errorcode': 405, 'errormsg': '请求方式错误'}
-    return HttpResponse(json.dumps(resp), status=405, reason='Http Method Error', content_type='application/json')
+    resp = {'code': 10000, 'errorMsg': '请求方式错误'}
+    return HttpResponse(json.dumps(resp), reason='Http Method Error', content_type='application/json')
 
+
+def query_or_delete(request, id, _):
+    print("=======query_or_delete=======")
+    if request.method == 'DELETE' or request.method == 'delete':
+        try:
+            res = delete_by_id(request, id)
+            return res
+        except Exception as e:
+            return HttpResponse(json.dumps({'code': 10000, 'errorMsg': 'user not exist'}),
+                                content_type='application/json')
+    if request.method == 'GET' or request.method == 'get':
+        try:
+            res = get_user_by_id(request, id)
+            return res
+        except Exception as e:
+            return HttpResponse(json.dumps({'code': 10000, 'errorMsg': 'user not exist'}),
+                                content_type='application/json')
+
+    resp = {'code': 10000, 'errorMsg': '请求方式错误'}
+    return HttpResponse(json.dumps(resp), reason='Http Method Error', content_type='application/json')
 
 def create(request):
     body_unicode = request.body.decode('utf-8')
     body = json.loads(body_unicode)
 
-    baseErrResp = {'errorcode': 400}
+    baseErrResp = {'code': 10000}
 
     user = User()
     paramValid = True
     if 'name' not in body:
-        baseErrResp['errormsg'] = '缺少name参数'
+        baseErrResp['errorMsg'] = '缺少name参数'
         paramValid = False
 
     if paramValid and 'age' not in body:
-        baseErrResp['errormsg'] = '缺少age参数'
+        baseErrResp['errorMsg'] = '缺少age参数'
         paramValid = False
 
     if not paramValid:
-        return HttpResponse(json.dumps(baseErrResp), status=200, content_type='application/json')
+        return HttpResponse(json.dumps(baseErrResp), content_type='application/json')
 
     user.name = body['name']
     user.age = body['age']
@@ -45,20 +75,24 @@ def create(request):
         user.phone = body['phone']
     if 'description' in body:
         user.description = body['description']
-    user.save()
 
-    resp = {'errorcode': 200}
-    return HttpResponse(json.dumps(resp), status=200, content_type='application/json')
+    user.save()
+    resp = {'code': 0, 'errorMsg': ''}
+    return HttpResponse(json.dumps(resp), content_type='application/json')
 
 
 def get_user_by_id(request, uid):
     if request.method == 'GET' or request.method == 'get':
-        user = User.objects.get(id=uid)
-        if user is None:
-            return JsonResponse({'errorcode': 200, 'data': {}})
-        return JsonResponse({'errorcode': 200, 'data': model_to_dict(user)})
+        try:
+            user = User.objects.get(id=uid)
+            if user is None:
+                return JsonResponse({'code': 10000, 'data': {}, 'errorMsg': ''})
+            return JsonResponse({'code': 0, 'data': model_to_dict(user)})
+        except Exception as e:
+            return HttpResponse(json.dumps({'code': 10000, 'errorMsg': 'user not exist'}),
+                                content_type='application/json')
     else:
-        resp = {'errorcode': 405, 'errormsg': '请求方式错误'}
+        resp = {'code': 10000, 'errorMsg': '请求方式错误'}
         return JsonResponse(data=resp)
 
 
@@ -66,16 +100,17 @@ def update_by_id(request):
     body_unicode = request.body.decode('utf-8')
     body = json.loads(body_unicode)
 
-    baseErrResp = {'errorcode': 400}
+    baseErrResp = {'code': 10000}
     if 'id' not in body:
-        baseErrResp['errormsg'] = '缺少id参数'
+        baseErrResp['errorMsg'] = '缺少id参数'
         return JsonResponse(baseErrResp)
 
     if 'description' not in body and 'name' not in body and 'age' not in body and 'email' not in body and 'phone' not in body:
-        baseErrResp['errormsg'] = '缺少要跟新的目标参数'
+        baseErrResp['errorMsg'] = '缺少要跟新的目标参数'
         return JsonResponse(baseErrResp)
 
     user = User.objects.get(id=body['id'])
+
     if 'description' in body:
         user.description = body['description']
     if 'name' in body:
@@ -86,9 +121,9 @@ def update_by_id(request):
         user.email = body['email']
     if 'phone' in body:
         user.phone = body['phone']
-    user.save()
 
-    resp = {'errorcode': 200, 'errormsg': 'OK'}
+    user.save()
+    resp = {'code': 0, 'errorMsg': 'OK'}
     return JsonResponse(resp)
 
 
@@ -96,19 +131,9 @@ def delete_by_id(request, id):
     if request.method == 'DELETE' or request.method == 'delete':
         user = User.objects.get(id=id)
         user.delete()
-        resp = {'errorcode': 200, 'errormsg': '删除成功'}
+        resp = {'code': 0, 'errorMsg': '删除成功'}
         return HttpResponse(json.dumps(resp), content_type='application/json')
     else:
-        resp = {'errorcode': 405, 'errormsg': '请求方式错误'}
-        return HttpResponse(json.dumps(resp), status=405, reason='Http Method Error',
+        resp = {'code': 10000, 'errorMsg': '请求方式错误'}
+        return HttpResponse(json.dumps(resp), reason='Http Method Error',
                             content_type='application/json')
-
-
-def query_or_delete(request, id, _):
-    print("=======query_or_delete=======")
-    if request.method == 'DELETE' or request.method == 'delete':
-        return delete_by_id(request, id)
-    if request.method == 'GET' or request.method == 'get':
-        return get_user_by_id(request, id)
-    resp = {'errorcode': 405, 'errormsg': '请求方式错误'}
-    return HttpResponse(json.dumps(resp), status=405, reason='Http Method Error', content_type='application/json')
